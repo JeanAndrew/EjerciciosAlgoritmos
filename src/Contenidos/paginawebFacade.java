fc0f7a2cc3e534c630d56f56930f06deb0818525
PC-90949 <victor.cespedes0710@gmail.com>
2023-03-30 17:28:33 -0500

    Inserta imagenes

--- src/Contenidos/paginawebFacade.java
+++ src/Contenidos/paginawebFacade.java
@@ -8,12 +8,13 @@ public class paginawebFacade {
 	    public paginawebFacade() {
 	    	  List<Section> sections = new ArrayList<>();
 	          sections.add(new Section("Contenido"));
-	          
 	          sections.add(new Deportes("Deportes"));
 	          sections.add(new Farandolas("Farándula"));
 	          sections.add(new Investigacion("Investigación"));
 	         
 	          this.pagina = new BasicWebpagina("Mi página web", sections);
+                  
+	          this.pagina = new Imagenes(pagina);
 	          this.pagina = new AdsWebPaginaDecorator(pagina);
 	          this.pagina = new SocialMediaWebPaginaDecorato(pagina);
 	      }
